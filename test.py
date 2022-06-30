982566c7494b0b2ea20890dbe6fff12b41147375
MURAKI Taichi <muraki.taichi@gmail.com>
2022-06-30 11:11:36 +0900

    f

---
+++ test.py
@@ -0,0 +1,30 @@
+from fastai.vision.all import *
+from fastai import *
+from fastai.vision import *
+
+# from fastai.callbacks.hooks import *
+# from fastai.callbacks import SaveModelCallback
+import matplotlib.pyplot as plt
+import matplotlib.image as immg
+import gc
+import numpy as np
+import random
+from PIL import Image
+import warnings
+
+# warnings.filterwarnings("ignore")
+# sns.set_style('darkgrid')
+base_dir = Path("train300/")
+# path.ls()
+# print(path.ls())
+from fastai.vision.data import SegmentationDataLoaders
+
+fnames = get_files(base_dir / "train_images/")
+fnames_mask = get_files(base_dir / " train_masks/")
+dls = SegmentationDataLoaders.from_label_func(
+    base_dir,
+    bs=8,
+    fnames=get_files(base_dir / "train_images/"),
+    label_func=lambda o: base_dir / "train_masks" / f"{o.stem}.png",
+    codes=np.array(["background", "hand", "mat"], dtype=str),
+)
